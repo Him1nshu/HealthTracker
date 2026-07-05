e8cdc168dfa7d76bc58600e4cba6ccb7853e1b54
Him1nshu <astng888@gmail.com>
2026-07-05 23:43:31 +0530

    made changes to files for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+});
 
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('ht_token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("ht_token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
 });
 
 export default api;
